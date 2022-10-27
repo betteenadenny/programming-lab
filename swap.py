ae6ab1fd3a1cf19f6319535e547cb7d2cbe9c818
betteenadenny <116335920+betteenadenny@users.noreply.github.com>
2022-10-27 13:46:43 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,7 +1,5 @@
 a=int(input("Enter your first number:"))
 b=int(input("Enter your second number:"))
 print("Before swapping values are a=",a,"and b=",b)
-a=a+b
-b=a-b
-a=a-b
+a,b=b,a
 print("After swapping values are a=",a,"and b=",b)
